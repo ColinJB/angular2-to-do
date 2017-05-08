a605c85d611d654fb1ef9ab6cd74e4f5482d335b
Colin Bloom <colinjbloom113@gmail.com>
2017-05-08 11:54:24 -0700

    commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,10 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
-    <ul>
-      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-    </ul>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+    <hr>
+    <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+    <new-task (newTaskSender)="addTask($event)"></new-task>
   </div>
   `
 })
@@ -19,16 +21,24 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+
+  selectedTask = null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course',3),
+    new Task('Begin brainstorming possible JavaScript group projects',2),
+    new Task('Add README file to last few Angular repos on GitHub',2)
   ];
-}
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string) {
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
 
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
